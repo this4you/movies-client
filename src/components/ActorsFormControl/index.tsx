b947d9f700f41e08aabb2f75a58071dbaad2405b
V.Mertvichenko <v.mertvichenko@tsintegr.com>
2022-05-15 22:05:57 +0300

    delete actors

--- src/components/ActorsFormControl/index.tsx
+++ src/components/ActorsFormControl/index.tsx
@@ -1,7 +1,7 @@
 import { Add } from '@mui/icons-material';
-import { Stack, TextField, IconButton } from '@mui/material';
+import { Stack, TextField, IconButton, Box, Chip } from '@mui/material';
 import React, { FC } from 'react';
-
+import "./ActorsFormControl.scss";
 type ActorsFormControlProps = {
     setActors: Function,
     actors: Array<string>
@@ -13,7 +13,7 @@ const ActorsFormControl: FC<ActorsFormControlProps> = ({ setActors, actors }) =>
     const [actorName, setActorName] = React.useState<string>("");
 
     const onKeyDown = (event) => {
-        if (event.keyCode == 13) {
+        if (event.keyCode === 13) {
             event.preventDefault();
             addActorHandler();
         }
@@ -26,16 +26,26 @@ const ActorsFormControl: FC<ActorsFormControlProps> = ({ setActors, actors }) =>
         setActorName("");
     }
 
+    const deleteActor = (actorName) => {
+        setActors(actors.filter(a => a != actorName));
+    }
+
     return (
-        <Stack direction="row" alignItems="center" spacing={2}>
-            <TextField id="outlined-basic" className="form-input" label="Actor`s name" variant="outlined" onKeyDown={onKeyDown}
-                value={actorName} onChange={(event) => setActorName(event.target.value)} />
+        <>
+            <Stack direction="row" alignItems="center" spacing={2}>
+                <TextField id="outlined-basic" className="form-input" label="Actor`s name" variant="outlined" onKeyDown={onKeyDown}
+                    value={actorName} onChange={(event) => setActorName(event.target.value)} />
 
-            <IconButton onClick={addActorHandler} style={{ marginTop: "-17px" }} aria-label="delete">
-                <Add />
-            </IconButton>
+                <IconButton onClick={addActorHandler} style={{ marginTop: "-17px" }} aria-label="delete">
+                    <Add />
+                </IconButton>
 
-        </Stack>
+            </Stack>
+            <Box maxHeight={"82px"} overflow="auto" className="form-input" padding="10px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                {actors.length > 0 ? actors.map(value =>
+                    <Chip onClick={() => deleteActor(value)} className="actor-name" key={value} label={value} />) : <h4>No actors</h4>}
+            </Box>
+        </>
     )
 }
 
--- src/components/MovieForm/index.tsx
+++ src/components/MovieForm/index.tsx
@@ -1,8 +1,8 @@
 import React, { FC, ReactElement } from 'react';
 import { useForm } from "react-hook-form";
-import { ImportExport, Add } from '@mui/icons-material';
+import { ImportExport } from '@mui/icons-material';
 import SendIcon from '@mui/icons-material/Send';
-import { Box, Button, Chip, FormControl, IconButton, InputLabel, MenuItem, Paper, Select, Stack, TextField } from '@mui/material';
+import { Button, FormControl, InputLabel, MenuItem, Paper, Select, Stack, TextField } from '@mui/material';
 import "./MovieForm.scss"
 import { MovieModel } from '../../models';
 import { useMovies } from '../../hooks';
@@ -17,15 +17,8 @@ type MovieFormProps = {
 const MovieForm: FC<MovieFormProps> = ({ submitHandle, cinemaFormats }): ReactElement => {
 
   const { register, reset, handleSubmit, formState: { errors } } = useForm<MovieModel>();
-
   const { createMovie, fetchMovies } = useMovies();
-
-  const onKeyDown = (event) => {
-    if (event.keyCode == 13) {
-      event.preventDefault();
-      addActorHandler();
-    }
-  }
+  const [actors, setActors] = React.useState<string[]>([]);
 
   const onSubmit = handleSubmit(data => {
     data = actors ? { ...data, actors } : data;
@@ -37,72 +30,54 @@ const MovieForm: FC<MovieFormProps> = ({ submitHandle, cinemaFormats }): ReactEl
     });
   });
 
-  const [actors, setActors] = React.useState<string[]>([]);
-  const [actorName, setActorName] = React.useState<string>("");
 
-  const addActorHandler = () => {
-    if (actorName && !actors.includes(actorName)) {
-      setActors([actorName, ...actors]);
-    }
-    setActorName("");
-  }
+  return (
+    < Paper elevation={3} className="form" >
+      <div className="title">
+        <h2>Add new cinema</h2>
+      </div>
+      <div className="fields">
+        <form onSubmit={onSubmit}>
 
-  return (<Paper elevation={3} className="form">
-    <div className="title">
-      <h2>Add new cinema</h2>
-    </div>
-    <div className="fields">
-      <form onSubmit={onSubmit}>
+          <TextField id="title" name="title" className="form-input" label="Title" variant="outlined"
+            {...register("title", { required: true, maxLength: 20 })}
+            error={!!errors.title}
+          />
 
-        <TextField id="title" name="title" className="form-input" label="Title" variant="outlined"
-          {...register("title", { required: true, maxLength: 20 })}
-          error={!!errors.title}
-        />
+          <TextField type="number" id="year" name="year" className="form-input" label="Year" variant="outlined"
+            {...register("year", { required: true, maxLength: 4, min: 1895, max: new Date().getFullYear() })}
+            error={!!errors.year}
+          />
 
-        <TextField type="number" id="year" name="year" className="form-input" label="Year" variant="outlined"
-          {...register("year", { required: true, maxLength: 4, min: 1895, max: new Date().getFullYear() })}
-          error={!!errors.year}
-        />
+          <FormControl fullWidth className="form-input">
+            <InputLabel id="format-select-label">Format</InputLabel>
+            <Select error={!!errors.format}
+              {...register("format", { required: true })}
+              labelId="format-select-label"
+              id="format-select"
+              label="Format"
 
-        <FormControl fullWidth className="form-input">
-          <InputLabel id="format-select-label">Format</InputLabel>
-          <Select error={!!errors.format}
-            {...register("format", { required: true })}
-            labelId="format-select-label"
-            id="format-select"
-            label="Format"
-
-            defaultValue={""}
-          >
-            {cinemaFormats.map((format) => <MenuItem value={format} key={format}>{format}</MenuItem>)}
-          </Select>
-        </FormControl>
+              defaultValue={""}
+            >
+              {cinemaFormats.map((format) => <MenuItem value={format} key={format}>{format}</MenuItem>)}
+            </Select>
+          </FormControl>
 
 
-        <ActorsFormControl actors={actors} setActors={setActors} />
-        {/* <Stack direction="row" alignItems="center" spacing={2}>
-          <TextField id="outlined-basic" className="form-input" label="Actor`s name" variant="outlined" onKeyDown={onKeyDown}
-            value={actorName} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setActorName(event.target.value)} />
-          <IconButton onClick={addActorHandler} style={{ marginTop: "-17px" }} aria-label="delete">
-            <Add />
-          </IconButton>
-        </Stack> */}
+          <ActorsFormControl actors={actors} setActors={setActors} />
 
-        <Box maxHeight={"82px"} overflow="auto" className="form-input" padding="10px" sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-          {actors.length > 0 ? actors.map(value => <Chip key={value} label={value} />) : <h4>No actors</h4>}
-        </Box>
-
-        <Stack direction="row" spacing={3}>
-          <Button variant="outlined" startIcon={<ImportExport />}>
-            Import
-          </Button>
-          <Button variant="contained" type="submit" endIcon={<SendIcon />}>
-            Create
-          </Button>
-        </Stack>
-      </form>
-    </div>
-  </Paper>)
+          <Stack direction="row" spacing={3}>
+            <Button variant="outlined" startIcon={<ImportExport />}>
+              Import
+            </Button>
+            <Button variant="contained" type="submit" endIcon={<SendIcon />}>
+              Create
+            </Button>
+          </Stack>
+        </form>
+      </div>
+    </Paper >
+  )
 }
 
 export default MovieForm;
